11a8aad4e7ea6ac67d5df6d96a9d950c7cb4cd4a
CodeSmell <mbarlotta@gmail.com>
2017-01-05 15:26:42 -0500

    clean up format

--- annotation-user/src/main/java/codesmell/annotation/handler/FooBarAnnotationRuntimeHandler.java
+++ annotation-user/src/main/java/codesmell/annotation/handler/FooBarAnnotationRuntimeHandler.java
@@ -15,16 +15,15 @@ public class FooBarAnnotationRuntimeHandler {
 	public void annotationProcess() throws Exception {
 		// find the class w/ an annotation
 		ClassLoader cl = getClass().getClassLoader();
-	    Set<ClassPath.ClassInfo> classesInPackage = ClassPath.from(cl).getTopLevelClassesRecursive("codesmell");
-	    classesInPackage.stream()
-	    	.map(classInfo -> loadClass(classInfo))
-	    	.map(klass -> fooBarMethods(klass))
-	    	.flatMap(List::stream)
-	      	.forEach(fooBarInfo -> {
-	      		fooBarInfo.invoke();
+		Set<ClassPath.ClassInfo> classesInPackage = ClassPath.from(cl).getTopLevelClassesRecursive("codesmell");
+		classesInPackage.stream()
+			.map(classInfo -> loadClass(classInfo))
+			.map(klass -> fooBarMethods(klass))
+			.flatMap(List::stream).forEach(fooBarInfo -> {
+				fooBarInfo.invoke();
 			});
 	}
-	
+
 	public Class loadClass(ClassPath.ClassInfo classInfo) {
 		Class klass = null;
 		try {
@@ -35,31 +34,31 @@ public class FooBarAnnotationRuntimeHandler {
 		}
 		return klass;
 	}
-	
+
 	public List<FooBarHolder> fooBarMethods(Class klass) {
 		Method[] methods = klass.getMethods();
 		return Arrays.stream(methods)
-			.filter(method-> method.isAnnotationPresent(FooBar.class))
-			.map(method -> new FooBarHolder(klass, method))
-			.collect(Collectors.toList());
+				.filter(method -> method.isAnnotationPresent(FooBar.class))
+				.map(method -> new FooBarHolder(klass, method))
+				.collect(Collectors.toList());
 	}
-	
+
 	public final class FooBarHolder {
 		private final Class klass;
 		private final Method method;
-		
+
 		public FooBarHolder(Class klass, Method method) {
 			this.klass = klass;
 			this.method = method;
 		}
-		
+
 		public void invoke() {
 			try {
 				System.out.println(method.invoke(klass.getConstructor().newInstance()));
 			} catch (Exception e) {
-				throw new RuntimeException(e.getMessage(),e);
+				throw new RuntimeException(e.getMessage(), e);
 			}
 		}
 	}
-	
+
 }
